08e8ace6eed66c58f956d82fd1b72b56190b4e16
alyssame <pemiga.me@gmail.com>
2017-01-27 13:00:07 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = 
+			$messages = [
 							[
 								'type' => 'text',
 								'text' => $text
@@ -29,8 +29,8 @@ if (!is_null($events['events'])) {
 							[
 								'type' => 'text',
 								'text' => "ว่าไงงงงงงงงงงง"
-							];
-						
+							],
+						];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
