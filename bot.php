c74fc3b8f9e3f658c3ad520a85562f8936edf1f2
alyssame <pemiga.me@gmail.com>
2017-01-27 11:54:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,9 +18,21 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
+		
+			
+			$messages:[
+				{
+					'type' => 'text',
+					'text' => $text
+				},
+				{
+					"type":"text",
+					"text":"Hello, user"
+				},
+				{
+					"type":"text",
+					"text":"May I help you?"
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
